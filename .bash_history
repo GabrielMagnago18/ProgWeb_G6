492ff1e7b94188fc6db67bb365bca87671710661
ViniFontes73 <viniciusfontesads@gmail.com>
2023-10-28 21:03:50 +0000

    Meu commit pelo ssh

---
+++ .bash_history
@@ -0,0 +1,43 @@
+ls
+composer create-project codeigniter4/appstarter ProgWeb_g6
+ls
+cd ProgWeb_g6/
+ls
+pwd
+ls
+cd app/
+ls
+cd Config/
+ls
+vim App.php 
+cd ..
+ls
+cd ..
+ls
+vim env 
+mv env .env
+ls
+chmod -R 777 writable/
+ls -l
+cd ..
+ls
+pw
+ls
+cd ProgWeb_g6/
+ls
+cd app/Config/
+ls
+nano App.php 
+cd ..
+ls
+mv ProgWeb_g6/ ProgWeb_g6.erro
+ls
+mkdir grupo
+mv index.html grupo/
+mv tailwind.config.js grupo/
+cd grupo/
+ls
+exit
+ls
+cd CodeIgniter
+ls
---
+++ grupo/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Grupo 06</title>
+<script src="https://cdn.tailwindcss.com"></script>
+</script>
+</head>
+<body>
+<div class="w-screen h-screen bg-gradient-to-t from-gray-950 to-blue-600 flex flex-col items-center p-20 ">
+<div>
+    <h1 class="text-4x1 font-extrabold p-4">GRUPO 06</h1>
+</div>
+<div>
+    <table class="w-1/2 mx-auto border border-collapse"></tableclass>
+        <tr class="bg-gray-800 text-white">
+        <th class="p-5" >Membros</th>
+        <th class="p-3">Matrícula</th>
+        </tr>
+        <tr class="bg-gray-200 hover:bg-gray-300">
+            <td class="">Maria Eduarda</td>
+            <td class="text-center">25384</td>
+        </tr>
+        <tr class="bg-gray-200 hover:bg-gray-300">
+        
+            <td>Gabriel Batista</td>
+            <td class="text-center">25387</td>
+        </tr>
+        <tr class="bg-gray-200 hover:bg-gray-300">
+            <td>Vinicius Fontes</td>
+            <td class="text-center">25388</td>
+        </tr>   
+    </table>
+</div>
+
+
+
+
+
+
+</div>
+
+</body>
+</html>
